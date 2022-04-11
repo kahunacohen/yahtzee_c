7d9d562ee3b0766533ed11148a0f5049cb9548d8
Aaron Cohen <acohen@mountainpasstech.com>
2022-04-11 23:11:07 +0300

    --no-verify

--- tests/test_roll.c
+++ tests/test_roll.c
@@ -3,34 +3,33 @@
 #include <stdbool.h>
 #include "../src/turn.h"
 
-// START_TEST (roll_dice_test) {
-//     init_dice();
-//     bool which_dice[MAX_DICE_PER_ROLL] = {false, false, true, false, false};
-//     Roll roll = roll_dice(which_dice);
-//     debug_dice_rolled(&roll);
-//     fail_unless(roll.num_dice == 1);
-//     fail_unless(roll.dice[0] > 0 && roll.dice[0] <= 6);
-
-// }
-END_TEST
 START_TEST (take_turn_test) {
-    init_dice();
-    //bool which_dice[MAX_DICE_PER_ROLL] = {true, true, true, true, true};
+    // Take a turn, rolling all 5 dice. Don't seed the random
+    // numbers so we can test.
     Turn turn;
     take_turn(&turn, 0, (bool[]){true, true, true, true, true});
     for (int i = 0; i <= MAX_DICE_PER_ROLL - 1; i++) {
-        printf("rolled: %d\n", turn.rolls[0].dice[i]);
+        // Confirm that all the rolled dice are between 1-6
+        int* first_rolled_dice = turn.rolls[0].dice;
+        fail_unless(first_rolled_dice[i] >=1 && first_rolled_dice[i] <= 6);
     }
     printf("\n");
     // debug_roll(&turn.rolls[0]);
+    // Take a second turn, rolling the first and last dice.
     take_turn(&turn, 1, (bool[]){true, false, false, false, true});
     for (int i = 0; i <= MAX_DICE_PER_ROLL - 1; i++) {
-        printf("rolled: %d\n", turn.rolls[1].dice[i]);
+        // Confirm the first and last die are different than the first.
+        int* first_rolled_dice = turn.rolls[0].dice;
+        int* second_rolled_dice = turn.rolls[1].dice;
+        // The first and last dice should be different.
+        if (i == 0 || i == 4) {
+            fail_unless(first_rolled_dice[i] != second_rolled_dice[i]);
+        } else {
+            // The dice not in the first or last position should be the same
+            // since we didn't roll them from the last turn.
+            fail_unless(first_rolled_dice[i] == second_rolled_dice[i]);
+        }
     }
-    
-    // debug_roll(&turn.rolls[1]);
-
-
 }
 END_TEST
 
