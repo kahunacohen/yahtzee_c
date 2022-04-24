9b75dad271a0a2a48728ee94cd9a61606c78e894
Aaron Cohen <acohen@mountainpasstech.com>
2022-04-24 23:19:56 +0300

    add test for take_turn

--- tests/test_roll.c
+++ tests/test_roll.c
@@ -14,33 +14,35 @@ START_TEST (init_turn_test) {
     }
     free(turn);
 } END_TEST
-// START_TEST (take_turn_test) {
-//     // Take a turn, rolling all 5 dice. Don't seed the random
-//     // numbers so we can test.
-//     Turn turn;
-//     take_turn(&turn, 0, (bool[]){true, true, true, true, true});
-//     for (int i = 0; i <= MAX_DICE_PER_ROLL - 1; i++) {
-//         // Confirm that all the rolled dice are between 1-6
-//         int* first_rolled_dice = turn.rolls[0].dice;
-//         fail_unless(first_rolled_dice[i] >=1 && first_rolled_dice[i] <= 6);
-//     }
-//     // Take a second turn, rolling the first and last dice.
-//     take_turn(&turn, 1, (bool[]){true, false, false, false, true});
-//     for (int i = 0; i <= MAX_DICE_PER_ROLL - 1; i++) {
-//         // Confirm the first and last die are different than the first.
-//         int* first_rolled_dice = turn.rolls[0].dice;
-//         int* second_rolled_dice = turn.rolls[1].dice;
-//         // The first and last dice should be different.
-//         if (i == 0 || i == 4) {
-//             fail_unless(first_rolled_dice[i] != second_rolled_dice[i]);
-//         } else {
-//             // The dice not in the first or last position should be the same
-//             // since we didn't roll them from the last turn.
-//             fail_unless(first_rolled_dice[i] == second_rolled_dice[i]);
-//         }
-//     }
-// }
-// END_TEST
+START_TEST (take_turn_test) {
+    // Take a turn, rolling all 5 dice. Don't seed the random
+    // numbers so we can test.
+    Turn * turn = init_turn();
+
+    take_turn(turn, 0, (bool[]){true, true, true, true, true});
+    for (int i = 0; i <= MAX_DICE_PER_ROLL - 1; i++) {
+        // Confirm that all the rolled dice are between 1-6
+        int* first_roll_dice = turn->rolls[0].dice;
+        fail_unless(first_roll_dice[i] >=1 && first_roll_dice[i] <= 6);
+    }
+    // Take a second turn, rolling the first and last dice.
+    take_turn(turn, 1, (bool[]){true, false, false, false, true});
+    for (int i = 0; i <= MAX_DICE_PER_ROLL - 1; i++) {
+        // Confirm the first and last die are different than the first.
+        int* first_roll_dice = turn->rolls[0].dice;
+        int* second_roll_dice = turn->rolls[1].dice;
+        // The first and last dice should be different.
+        if (i == 0 || i == 4) {
+            fail_unless(first_roll_dice[i] != second_roll_dice[i]);
+        } else {
+            // The dice not in the first or last position should be the same
+            // since we didn't roll them from the last turn.
+            fail_unless(first_roll_dice[i] == second_roll_dice[i]);
+        }
+    }
+    free(turn);
+}
+END_TEST
 
 // START_TEST (test_is_yahtzee) {
 //     Roll roll;
@@ -73,7 +75,7 @@ int main(void)
 
     suite_add_tcase(s1, tc1_1);
     tcase_add_test(tc1_1, init_turn_test);
-    // tcase_add_test(tc1_1, take_turn_test);
+    tcase_add_test(tc1_1, take_turn_test);
     // tcase_add_test(tc1_1, test_is_yahtzee);
 
 
